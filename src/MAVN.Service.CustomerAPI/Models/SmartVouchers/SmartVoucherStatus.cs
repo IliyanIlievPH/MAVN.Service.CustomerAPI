c8b9364a903feecefeb396ffe1d5f6e86dfec3a4
Iliyan Iliev <iliyan.iliev@primeholding.com>
2020-06-15 12:04:06 +0300

    Add missing used status for smart voucher

--- src/MAVN.Service.CustomerAPI/Models/SmartVouchers/SmartVoucherStatus.cs
+++ src/MAVN.Service.CustomerAPI/Models/SmartVouchers/SmartVoucherStatus.cs
@@ -7,12 +7,14 @@
     {
         /// <summary>Unspecified status.</summary>
         None,
-        /// <summary>Indicates that the voucher is stock.</summary>
+        /// <summary>Indicates that the voucher is in stock.</summary>
         InStock,
-        /// <summary>Indicated that the voucher reserved by customer and waiting for payment.</summary>
+        /// <summary>Indicated that the voucher is reserved by customer and waiting for payment.</summary>
         Reserved,
-        /// <summary>Indicates that the voucher bought by a customer.</summary>
+        /// <summary>Indicates that the voucher is bought by a customer.</summary>
         Sold,
+        /// <summary>Indicates that the voucher is used by a customer.</summary>
+        Used,
         /// <summary>Indicates that the voucher has expired.</summary>
         Expired,
     }
